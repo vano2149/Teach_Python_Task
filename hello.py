a7cf855cb3ea24df58195b425a5147dd1ecd4f69
Kal'ko Ivan <vano2149@gmail.com>
2022-04-19 17:02:22 +0300

    Повтор python-а!!

---
+++ hello.py
@@ -0,0 +1,21 @@
+
+
+m = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+]
+
+diag = [m[i][i] for i in [0, 1, 2]]
+print(diag)
+print(m)
+
+doubles = [c * 2 for c in 'spam']
+print(doubles)
+
+G = (sum(row) for row in m)
+print(next(G), next(G), next(G))
+a = {1,'f',2,3,'ghb','хуй'}
+print(a)
+
+# стр 208.
