8b60f183c52164143548182eb095da6daa0bb5c6
Kal'ko Ivan <vano2149@gmail.com>
2022-04-21 14:08:21 +0300

    Modified in an echo.py file!!!

--- echo.py
+++ echo.py
@@ -1,8 +1,24 @@
 """
- стр. 285 Прмер базы данных о фильмах!
+ стр. 302 Проверь свои знания: Контрольные вопросы
 """
 
-import sys
+rec = {'name' : 'Bob',
+        'jobs' : ['developer', 'manager'],
+        'web' : 'www.bobs.org/Bob',
+        'home': {'state': 'Overworked', 'zip': 12345}}
+print(rec)
+print('-' * 30)
 
-print(sys.argv[1:])
+print(rec['name'])
+print('-' * 30)
+print(rec['jobs'])
+print('-' * 30)
+print(rec['jobs'][1])
+print('-' * 30)
+print(rec['home']['zip'])
 
+db = []
+
+db.append(rec)
+e = db[0]['jobs']
+print(f'Достали работу из бд: {e}')
