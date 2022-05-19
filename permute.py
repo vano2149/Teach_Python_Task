f613f24c06ecb76a3882fe318425709616342145
Kal'ko Ivan <vano2149@gmail.com>
2022-05-19 10:35:04 +0300

    Page 640!

---
+++ permute.py
@@ -0,0 +1,27 @@
+"""
+Сраница 634!
+Суть здесь в том, что 
+генераторы иногда способны производить результаты
+"""
+
+def permute1(seq):
+    """"""
+    if not seq:
+        return [seq]
+    else:
+        res = []
+        for i in range(len(seq)):
+            rest = seq[:i] + seq[i+1:]
+            for x in permute1(rest):
+                res.append(seq[i:i+1]+x)
+        return res
+
+
+def permute2(seq):
+    if not seq:
+        yield seq
+    else:
+        for i in range(len(seq)):
+            rest = seq[:i] + seq[i+1:]
+            for x in permute2(rest):
+                yield seq[i:i+1] + x
